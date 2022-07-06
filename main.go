1992009a89959e4e173706c41e52e8844ee45048
nbyrd <zaf-khasanov@yandex.ru>
2022-07-06 11:09:32 +0300

    added log print

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"sync"
 	"time"
+	"os"
 
 	"golang.org/x/net/websocket"
 )
@@ -57,24 +58,30 @@ func action(left_fork, right_fork, id int, chanel chan string, forks []sync.Mute
 		if_first(id, &leftaction, &rightaction)
 
 		chanel <- format("phil", id, "think")
+		to_log("think", id, t_think)
 		time.Sleep(time.Duration(t_think) * time.Millisecond)
 		chanel <- format("phil", id, "wait")
 
 		/* philo takes forks and locks other routines */
 		forks[left_fork].Lock()
+		to_log("left lock", id, 0)
 		chanel <- format("fork" , left_fork, leftaction)
 		forks[right_fork].Lock()
+		to_log("right lock", id, 0)
 		chanel <- format("fork" , right_fork, rightaction)
 
 		/* philo takes a dinner for t_eat milliseconds */
 		chanel <- format("phil", id, "eat")
+		to_log("eat", id, t_eat)
 		time.Sleep(time.Duration(t_eat) * time.Millisecond)
 		chanel <- format("phil", id, "wait")
 
 		/* after having dinner philo unlocks forks (routines) */
 		forks[right_fork].Unlock()
+		to_log("right unlock", id, 0)
 		chanel <- format("fork" , right_fork, "back")
 		forks[left_fork].Unlock()
+		to_log("left unlock", id, 0)
 		chanel <- format("fork" , left_fork, "back")
 	}
 }
@@ -92,6 +99,30 @@ func if_first(id int, leftaction *string, rightaction *string){
 
 func genTime() int {
 	return rand.Intn(max_time+min_time) - min_time
+}
+
+func to_log(action string, id int, time int){
+	f, err := os.OpenFile("logs", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	switch action {
+		case "eat":
+			log.Printf("Philo %d is eating for %d millisecs\n", id, time)
+		case "think":
+			log.Printf("Philo %d is thinking for %d millisecs\n", id, time)
+		case "left lock":
+			log.Printf("Philo %d locks left fork\n", id)
+		case "right lock":
+			log.Printf("Philo %d locks right fork\n", id)
+		case "left unlock":
+			log.Printf("Philo %d unlocks left fork\n", id)
+		case "right unlock":
+			log.Printf("Philo %d unlocks right fork\n", id)
+	}
+	
 }
 
 func format(who string, id int, action string) string {
